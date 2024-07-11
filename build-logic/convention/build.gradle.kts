cbd44a8bca55148a805f02e64941008b2af26f33
Amr Gharseldin <gharseldin@aucegypt.edu>
2024-07-10 22:06:56 -0700

    Added more Convention Plugins
    - Added Library convention plugin
    - Added Library Compose convention plugin
    - Added Room Convention plugin
    - Added JVM Library Convention plugin
    - Added Ktor convention plugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -22,5 +22,29 @@ gradlePlugin {
             id = "tasky.android.application.compose"
             implementationClass = "AndroidApplicationComposeConventionPlugin"
         }
+        register("androidLibrary") {
+            id = "tasky.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+        register("androidLibraryCompose") {
+            id = "tasky.android.library.compose"
+            implementationClass = "AndroidLibraryComposeConventionPlugin"
+        }
+        register("androidFeatureUi") {
+            id = "tasky.android.feature.ui"
+            implementationClass = "AndroidFeatureUiConventionPlugin"
+        }
+        register("androidRoom") {
+            id = "tasky.android.room"
+            implementationClass = "AndroidRoomConventionPlugin"
+        }
+        register("jvmLibrary") {
+            id = "tasky.jvm.library"
+            implementationClass = "JvmLibraryConventionPlugin"
+        }
+        register("jvmKtor") {
+            id = "tasky.jvm.ktor"
+            implementationClass = "JvmKtorConventionPlugin"
+        }
     }
 }
